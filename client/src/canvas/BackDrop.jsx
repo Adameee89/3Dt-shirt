dce63179b8cbfd1a59e9e6c42fab8ae1a23d3881
Adam Vegvari <brokenskijas9@gmail.com>
2024-07-14 12:03:46 +0200

    client side done

--- client/src/canvas/BackDrop.jsx
+++ client/src/canvas/BackDrop.jsx
@@ -15,7 +15,7 @@ function BackDrop() {
     temporal
     frames={60}
     alphaTest={0.85}
-    scale={10}
+    scale={1}
     rotation={[Math.PI / 2, 0, 0]}
     position={[0, 0, -0.14]}
     >
@@ -24,14 +24,14 @@ function BackDrop() {
       radius={9} 
       ambient={0.25} 
       intensity={0.55}
-      position={[5, 5, -10]}  
+      position={[5, -5, -10]}  
       />
 
       <RandomizedLight 
       amount={4} 
       radius={5} 
       ambient={0.55} 
-      position={[-5, 5, -9]}  
+      position={[5, 5, -9]}  
       />
     </AccumulativeShadows>
   )
--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -10,7 +10,7 @@ function CanvasModel() {
   return (
     <Canvas 
     shadows 
-    camera={{position: [0, 0, 0], fov: 22}}
+    camera={{position: [0, 0, 0], fov: 23}}
     gl={{preserveDrawingBuffer: true}}
     className='w-full max-w-full h-full transition-all  ease-in'
     >
